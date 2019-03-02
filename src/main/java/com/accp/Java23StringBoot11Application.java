57418e6cd2ca5ceeaaa018e457c162e8d06dccde
DavidLikeBanana <1175063903@qq.com>
2019-03-02 09:06:07 +0800

    第一次commit

---
+++ src/main/java/com/accp/Java23StringBoot11Application.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java23StringBoot11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java23StringBoot11Application.class, args);
+	}
+
+}
